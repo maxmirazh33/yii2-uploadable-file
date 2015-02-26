7006c0ba005506e226b9fbcbddb9b319e88299bf
maxmirazh33 <maxmirazh33@gmail.com>
2015-02-26 22:31:53 +0600

    improve GetFileUrlTrait

--- GetFileUrlTrait.php
+++ GetFileUrlTrait.php
@@ -9,20 +9,20 @@ trait GetFileUrlTrait
      */
     public function getFileUrl($attr)
     {
-        if (mb_strpos(get_class($this), 'backend') === false) {
-            $class = new \ReflectionClass($this);
-            $class = 'backend\models\\' . $class->getShortName();
-            $model = new $class;
-            foreach ($model->behaviors as $b) {
-                if ($b instanceof Behavior) {
-                    return $b->getFileUrl($attr, $this);
-                }
+        foreach ($this->behaviors as $behavior) {
+            if ($behavior instanceof Behavior) {
+                return $behavior->getFileUrl($attr);
             }
         }
 
-        foreach ($this->behaviors as $b) {
-            if ($b instanceof Behavior) {
-                return $b->getFileUrl($attr);
+        $class = new \ReflectionClass($this);
+        $class = 'backend\models\\' . $class->getShortName();
+        if (class_exists($class)) {
+            $model = new $class;
+            foreach ($model->behaviors as $behavior) {
+                if ($behavior instanceof Behavior) {
+                    return $behavior->getFileUrl($attr, $this);
+                }
             }
         }
 
